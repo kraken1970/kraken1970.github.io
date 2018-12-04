0efcae6d7c9d000c9bc711199d5ff3aac2183e07
kraken1970 <everestdon@ukr.net>
2018-12-04 15:17:53 +0300

    files for todo sites

---
+++ build/precache-manifest.8938363e16947bdb649a9cb54fa93ed1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e5793d32bc54f41d9b50",
+    "url": "/static/js/main.e5793d32.chunk.js"
+  },
+  {
+    "revision": "bc951937f3a44b7747ac",
+    "url": "/static/js/1.bc951937.chunk.js"
+  },
+  {
+    "revision": "e5793d32bc54f41d9b50",
+    "url": "/static/css/main.2c307019.chunk.css"
+  },
+  {
+    "revision": "eb65e79dd76c38ac40aeaea3f89c8ba0",
+    "url": "/index.html"
+  }
+];
