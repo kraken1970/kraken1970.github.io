7bce01a0062817eb44cd1721c1f5bf4d5a4a93da
kraken1970 <everestdon@ukr.net>
2018-12-14 14:21:04 +0300

    Add files via upload

---
+++ precache-manifest.662f96c01c48ec21f1b961592b8bc992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0537fae4d1ea857f645d466625ae8bcd",
+    "url": "/static/media/p2.0537fae4.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "136331e1610892b47d85",
+    "url": "/static/js/main.136331e1.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "136331e1610892b47d85",
+    "url": "/static/css/main.3007105b.chunk.css"
+  },
+  {
+    "revision": "d606904fc482ca132250dba9c45569fd",
+    "url": "/index.html"
+  }
+];
